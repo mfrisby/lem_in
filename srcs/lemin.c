26b040d87825d8a66cd067d635d930ac3fa076a3
Morgane FRISBY <mfrisby@e1r9p7.42.fr>
2017-11-13 11:14:19 +0100

    RMV open ADD get_next_line fd = 0

--- srcs/lemin.c
+++ srcs/lemin.c
@@ -14,31 +14,31 @@
 #include "../includes/lemin.h"
 #include "../libft/includes/libft.h"
 
-static void		aff_lst(t_room *head)
-{
-	t_room		*tmp;
-	t_link		*link;
+// static void		aff_lst(t_room *head)
+// {
+// 	t_room		*tmp;
+// 	t_link		*link;
 
-	tmp = head;
-	while (tmp)
-	{
-		ft_printf("name: %s - role: %d - poid: %d\n"
-				, tmp->name, tmp->role, tmp->poid);
-		link = tmp->link;
-		if (link)
-			ft_putstr("    tubes: ");
-		while (link)
-		{
-			ft_putchar(' ');
-			ft_putstr(link->ptr->name);
-			if (link->next)
-				ft_putstr(" -");
-			link = link->next;
-		}
-		ft_putchar('\n');
-		tmp = tmp->next;
-	}
-}
+// 	tmp = head;
+// 	while (tmp)
+// 	{
+// 		ft_printf("name: %s - role: %d - poid: %d\n"
+// 				, tmp->name, tmp->role, tmp->poid);
+// 		link = tmp->link;
+// 		if (link)
+// 			ft_putstr("    tubes: ");
+// 		while (link)
+// 		{
+// 			ft_putchar(' ');
+// 			ft_putstr(link->ptr->name);
+// 			if (link->next)
+// 				ft_putstr(" -");
+// 			link = link->next;
+// 		}
+// 		ft_putchar('\n');
+// 		tmp = tmp->next;
+// 	}
+// }
 
 static void		init_data(t_data **data)
 {
@@ -49,37 +49,16 @@ static void		init_data(t_data **data)
 	(*data)->ants = NULL;
 }
 
-static int		open_map(char *file)
-{
-	int			fd;
-
-	fd = open(file, O_RDONLY);
-	return (fd);
-}
-
-int				main(int ac, char **av)
+int				main(void)
 {
 	t_data		*data;
-	int			fd;
 
-	fd = -1;
 	data = malloc(sizeof(t_data));
 	init_data(&data);
-	if (ac == 3 && (ft_strcmp(av[2], "--debug") != 0))
-		error_map();
-	if (ac != 2)
-		error_map();
-	if ((fd = open_map(av[1])) == -1)
-		return (-1);
-	parse_map(fd, &data);
+	parse_map(0, &data);
 	check_map(data);
 	if (get_path(data) == -1)
 		error_map();
-	if (ac == 3)
-	{
-		aff_lst(get_start_room(data->head));
-		ft_putchar('\n');
-	}
 	make_anthill(data);
 	go_fourmi(data);
 	return (0);
